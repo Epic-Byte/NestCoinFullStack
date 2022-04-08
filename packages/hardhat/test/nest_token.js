685915c7c7fe89b22ab95fab5e1fb147f1bff311
kingzamzon <adesanoye.samson@yahoo.com>
2022-04-08 13:46:31 +0100

    test for batch reward mint

--- packages/hardhat/test/nest_token.js
+++ packages/hardhat/test/nest_token.js
@@ -10,11 +10,11 @@ use(solidity);
 
 describe("NestToken 🤖", function () {
     let nestToken;
-    let owner, addr1, addr2, addrs;
+    let owner, addr1, addr2, addr3, addrs;
 
     beforeEach(async function () {
         // create the smart contract object to test from
-        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
         const NestToken = await ethers.getContractFactory("NestToken");
         nestToken = await NestToken.deploy();
     });
@@ -36,7 +36,20 @@ describe("NestToken 🤖", function () {
             expect(addr1Balance).to.equal(50);
         });
 
-        
+        it("should mint batch reward", async function () {
+            const addressList = [addr2.address, addr3.address];
+            const amountList = [20, 40];
+
+            await nestToken.BatchRewardMint(addressList, amountList);
+
+            // test address 2 balance
+            const addr2Balance = await nestToken.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(20);
+
+            // test address 3 balance
+            const addr3Balance = await nestToken.balanceOf(addr3.address);
+            expect(addr3Balance).to.equal(40);
+        })
     })
 
 });
